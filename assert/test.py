68dbf310f1e585bd7ff9078d6781484ff09e005f
Zoila <zoilavillatoro6694@outlook.com>
2020-05-14 21:01:30 -0600

    added use pytest and assert

---
+++ assert/test.py
@@ -0,0 +1,13 @@
+from algoritmos import fibonacci, palindromo, factorial
+
+#pytest es para cosas mas sencillas
+#todas las pruebas unitarias se haran con funciones
+# se comienza con la palabra test
+def test_fibonacci_cinco():
+	assert fibonacci(5) == 5
+
+def test_palindromo_anita():
+	assert palindromo("Anita lava la tina")
+
+def test_factorial_cinco():
+	assert factorial(5) == 120
